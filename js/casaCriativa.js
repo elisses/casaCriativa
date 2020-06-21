245446132b6ac11b6cf7babdae45f5cb3db24994
elis <fernandalisses@gmail.com>
2020-06-22 01:46:57 +0200

    implement click

--- js/casaCriativa.js
+++ js/casaCriativa.js
@@ -1,18 +1,23 @@
-function onOff() {
+const onOff = () => {
     document
         .querySelector('#modal')
         .classList
         .toggle("hide")
-    
+
     document
         .querySelector('body')
         .classList
         .toggle("hideScroll")
-        
+
     document
         .querySelector('#modal')
         .classList
         .toggle("addScroll")
-   
+
 };
 
+const newIdea = document.getElementById('newIdea')
+
+newIdea.addEventListener('click', (e) => {
+    window.open("./ideias.html");
+})
